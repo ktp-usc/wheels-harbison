60a577e29d68921bf763101302558608d8a356d4
bguliano <72895782+bguliano@users.noreply.github.com>
2025-10-20 20:46:42 -0400

    WHL-4 set up for static deployment on GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "wheels-harbison";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`,
 };
 
 export default nextConfig;
