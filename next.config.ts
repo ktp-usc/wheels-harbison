895296714add0b5ead297a521a4020c7505724cc
bguliano <72895782+bguliano@users.noreply.github.com>
2025-10-23 11:51:24 -0400

    remove assetPrefix and basePrefix from next.config.ts to correctly support custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const repo = "wheels-harbison";
-
 const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true,
-    },
-    basePath: `/${repo}`,
-    assetPrefix: `/${repo}`,
+    }
 };
 
 export default nextConfig;
