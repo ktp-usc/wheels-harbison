f80774deb4f9e3d1b4ca9e25e6840a9c77b8609e
Luke Jannazzo <143638098+jannazzo@users.noreply.github.com>
2025-11-03 10:05:48 -0500

    Cropped all images to 16:9. (#11)
    Converted all images to webp.
    Placed images in public folder instead of images.

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -1,16 +1,5 @@
 "use client"
 
-import Wheels1 from "@/images/Wheels1.jpg";
-import Wheels2 from "@/images/Wheels2.png";
-import Wheels3 from "@/images/Wheels3.png";
-import Wheels4 from "@/images/Wheels4.jpg";
-import Wheels5 from "@/images/Wheels5.jpg";
-import Wheels6 from "@/images/Wheels6.jpg";
-import Wheels7 from "@/images/Wheels7.jpg";
-import Wheels8 from "@/images/Wheels8.png";
-import Wheels9 from "@/images/Wheels9.jpg";
-import Wheels10 from "@/images/Wheels10.jpg";
-import Wheels11 from "@/images/Wheels11.jpg";
 import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import Image, {StaticImageData} from "next/image";
 
@@ -26,7 +15,19 @@ type Props = {
 };
 
 export default function Slideshow({
-                                      images = [Wheels1, Wheels2, Wheels3, Wheels4, Wheels5, Wheels6, Wheels7, Wheels8, Wheels9, Wheels10, Wheels11],
+                                      images = [
+                                          '/images/Wheels1.webp',
+                                          '/images/Wheels2.webp',
+                                          '/images/Wheels3.webp',
+                                          '/images/Wheels4.webp',
+                                          '/images/Wheels5.webp',
+                                          '/images/Wheels6.webp',
+                                          '/images/Wheels7.webp',
+                                          '/images/Wheels8.webp',
+                                          '/images/Wheels9.webp',
+                                          '/images/Wheels10.webp',
+                                          '/images/Wheels11.webp'
+                                      ],
                                       width = 1200,
                                       height = 700,
                                       autoplay = true,
