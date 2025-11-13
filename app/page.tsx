0026640031d9eb7f054069e3f4d4d32d75ec006a
galenmiller <galenjr68@gmail.com>
2025-11-13 12:58:00 -0500

    WHL-29 Made sure the header doesn't cover anything

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Slideshow from "@/components/Slideshow";
 
 export default function Page() {
     return (
-        <main className="relative pt-0">
+        <main className="relative pt-25">
             <div className="max-w-5xl mx-auto px-4 pt-10 mb-18 text-center">
                 <h2 className="text-4xl font-serif font-bold text-gray-900 mb-4">
                     Providing free transportation to elderly and disabled residents in the Greater Harbison Area
                 </h2>
-                <div className="w-[235px] h-1 bg-black mx-auto rounded mb-6"></div>
+                <div className="w-[935px] h-1 bg-black mx-auto rounded mb-6"></div>
 
                 <Slideshow />
             </div>
