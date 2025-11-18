a26c589390e55785a813708da46eab631e855965
galenmiller <galenjr68@gmail.com>
2025-11-18 17:44:03 -0500

    WHL-31 Updated the padding above headlines

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function WhoWeArePage() {
     return (
-        <main className="bg-gradient-to-b from-gray-100 to-gray-200 font-sans pb-24">
+        <main className="bg-gradient-to-b from-gray-100 to-gray-200 font-sans">
             {/* Our Mission */}
             <section>
                 <div className="max-w-6xl mx-auto px-6 py-16 text-center">
