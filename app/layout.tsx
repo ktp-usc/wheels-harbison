47e4ed5613f77eab7d6b4f61e80b3e095867df2c
wsraiford <wsraiford@icloud.com>
2025-11-18 13:38:23 -0500

    pinned footer to bottom of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,12 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
         >
         <Navbar />
-        {children}
-
-
+        <main className="flex-grow">
+            {children}
+        </main>
         <WheelsFooter />
 
         </body>
